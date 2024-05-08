3d20557e88e8cb8501c69dc96b72fe63bd59bce8
BERUK-GGG <s2206420@student.hb.se>
2024-05-08 15:31:39 +0200

    MVVM

--- PresentationLayer.MVVM/ViewModels/LoginModel.cs
+++ PresentationLayer.MVVM/ViewModels/LoginModel.cs
@@ -5,13 +5,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using Affärslager;
+using PresentationLayer.MVVM.Models;
+using PresentationLayer.MVVM.Views;
 
 namespace PresentationLayer.MVVM.ViewModels
 {
-    internal class LoginModel
+    public class LoginModel: ObservableObject
     {
-        private ICommand exitCommand = null!;
-        public ICommand ExitCommand =>
-        exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
+
+        TabellController tabell = new TabellController();
+
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        private string _error;
+        public string Error
+        {
+            get { return _error; }
+            set { _error = value; OnPropertyChanged(); }
+        }
+
+        private ICommand _loginCommand;
+        public ICommand LoginCommand =>
+            _loginCommand ??= new RelayCommand(Login);
+
+        private void Login()
+        {
+            if (Authenticate(Username, Password))
+            {
+                // Login successful
+                // You can perform any action here, such as navigating to another page
+                 
+                StartSida startSida = new StartSida();
+                startSida.Show();
+
+                
+
+            }
+            else
+            {
+                // Login failed
+                // You can display an error message here
+                Error = "Invalid username or password.";
+            }
+        }
+
+        public bool Authenticate(string Username, string Password)
+        {
+            var employee = tabell.MekanikerTabell().FirstOrDefault(a => a.AnvändarNamn == Username && a.lösenord == Password);
+            return employee != null;
+        }
     }
 }
---
+++ PresentationLayer.MVVM/Views/Login.xaml.cs
@@ -0,0 +1,67 @@
+﻿
+
+using Affärslager;
+using RB_Ärendesystem.Datalayer;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class Login : Window
+    {
+        TabellController tabell = new TabellController();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        //private void Login_Click(object sender, RoutedEventArgs e)
+        //{
+            
+        //    string username = UsernameTextBox.Text;
+        //    string password = PasswordBox.Password;
+            
+            
+        //    // Query the database to check if the username and password match
+
+        //    var employee = tabell.MekanikerTabell().FirstOrDefault(a => a.AnvändarNamn == username && a.lösenord == password);
+        //    // Perform authentication logic here
+            
+            
+        //    //if (username == "admin" && password == "password")
+        //    //{
+        //    //    // Successful login, navigate to the main window or perform other actions
+
+        //    //    StartSida startSida = new StartSida();
+        //    //    startSida.Show();
+        //    //    this.Close();
+        //    //}
+
+        //    //else if (employee != null)
+        //    //{
+        //    //    // Successful login, navigate to the main window or perform other actions
+        //    //    StartSida startSida = new StartSida();
+        //    //    startSida.Show();
+        //    //    this.Close();
+        //    //}
+
+        //    //else
+        //    //{
+        //    //    // Display error message
+        //    //    ErrorMessage.Text = "Invalid username or password.";
+        //    //}
+        //}
+    }
+}
