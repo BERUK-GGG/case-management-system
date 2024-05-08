93e13099f0bbd147a72cf9b3b2fd654cebb0a835
BERUK-GGG <s2206420@student.hb.se>
2024-05-08 13:49:05 +0200

    MVVM

---
+++ PresentationLayer.MVVM/ViewModels/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using PresentationLayer.MVVM.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PresentationLayer.MVVM.ViewModels
+{
+    internal class LoginModel
+    {
+        private ICommand exitCommand = null!;
+        public ICommand ExitCommand =>
+        exitCommand ??= exitCommand = new RelayCommand(() => App.Current.Shutdown());
+    }
+}
