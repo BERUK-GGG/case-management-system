33f5b1a2048982c38b50ae705deb8b90b10f0bde
BERUK-GGG <s2206420@student.hb.se>
2024-05-10 19:02:30 +0200

    RegistreraJournal MVVM, går inte välja från ListBoxen!

---
+++ PresentationLayer.MVVM/ViewModels/RegistreraJournalModel.cs
@@ -0,0 +1,155 @@
+﻿using Affärslager;
+using PresentationLayer.MVVM.Commands;
+using PresentationLayer.MVVM.Models;
+using PresentationLayer.MVVM.Views;
+using RB_Ärendesystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PresentationLayer.MVVM.ViewModels
+{
+    public class RegistreraJournalModel : ObservableObject
+    {
+        TabellController _tabellController = new TabellController();
+        NyJournalController _nyJournalController = new NyJournalController();
+        public RegistreraJournalModel()
+        {
+            // Initialize properties or fetch data here if needed
+
+            Bokningar = _tabellController.BesökTabell();
+            var journalItems = _tabellController.JournalTabell();
+
+            // Fetch the list of mechanics from the database
+            var reservdel = _tabellController.ReservdellTabell();
+
+            var reservdelIdsInJournal = journalItems.Select(j => j.ID);
+
+            // Filter reservdel based on ReservdelIds not present in the journalItems
+            var reservdelFiltered = reservdel.Where(r => !reservdelIdsInJournal.Contains(r.ID)).ToList();
+            Reservdelar = new List<Reservdel>(reservdelFiltered);
+        }
+
+
+
+
+        private ObservableCollection<Besök> _bokningar;
+        public ObservableCollection<Besök> Bokningar
+        {
+            get { return _bokningar; }
+            set { _bokningar = value; OnPropertyChanged(); }
+        }
+
+        private List<Reservdel> _reservdelar;
+        public List<Reservdel> Reservdelar
+        {
+            get { return _reservdelar; }
+            set { _reservdelar = value; OnPropertyChanged(); }
+        }
+
+        private string _besökID;
+        public string BesökID
+        {
+            get { return _besökID; }
+            set { _besökID = value; OnPropertyChanged(); }
+        }
+
+        private string _åtgärder;
+        public string Åtgärder
+        {
+            get { return _åtgärder; }
+            set { _åtgärder = value; OnPropertyChanged(); }
+        }
+
+        private Besök _selectedBesök;
+        public Besök SelectedBesök
+        {
+            get { return _selectedBesök; }
+            set
+            {
+                _selectedBesök = value;
+                OnPropertyChanged();
+                if (SelectedBesök != null)
+                {
+                    BesökID = SelectedBesök.ID.ToString();
+
+                }
+            }
+        }
+
+        private List<Reservdel> _selectedReservdelar;
+        public List<Reservdel>  SelectedReservdelar
+        {
+            get { return _selectedReservdelar; }
+            set { _selectedReservdelar = value ; OnPropertyChanged(); } 
+        }
+
+        private ICommand _sparaCommand;
+        public ICommand SparaCommand =>
+            _sparaCommand ??= new RelayCommand(Spara);
+
+        private void Spara()
+        {
+
+            if (SelectedBesök != null)
+            {
+                if (SelectedReservdelar != null && SelectedReservdelar.Any())
+                {
+                    List<Reservdel> selectedReserv = new List<Reservdel>();
+                    foreach (var selectedItem in SelectedReservdelar)
+                    {
+                        selectedReserv.Add((Reservdel)selectedItem);
+                    }
+                    _nyJournalController.AddJournal(åtgärder: Åtgärder, besök: SelectedBesök, Reservdelar: selectedReserv);
+                    MessageBox.Show("Data saved successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            else
+            {
+                MessageBox.Show("Please select at least one Reservdel.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+            else
+            {
+                MessageBox.Show("Please select a Besök.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+}
+        private ICommand _backCommand;
+        public ICommand BackCommand =>
+            _backCommand ??= new RelayCommand(Back);
+
+        private void Back()
+        {
+            StartSida startSida = new StartSida();
+            startSida.Show();
+
+            RegistreraJournal RegJournalPage = Application.Current.Windows.OfType<RegistreraJournal>().FirstOrDefault();
+            RegJournalPage?.Close();
+
+
+        }
+        private ICommand _logoutCommand;
+        public ICommand LogoutCommand =>
+            _logoutCommand ??= new RelayCommand(Logout);
+
+        private void Logout()
+        {
+            Login login = new Login();
+            login.Show();
+
+            RegistreraJournal RegJournalPage = Application.Current.Windows.OfType<RegistreraJournal>().FirstOrDefault();
+            RegJournalPage?.Close();
+
+
+
+        }
+    }
+   
+}
---
+++ PresentationLayer.MVVM/Views/RegistreraJournal.xaml.cs
@@ -0,0 +1,162 @@
+﻿
+using Affärslager;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using PresentationLayer.MVVM.ViewModels;
+using RB_Ärendesystem.Datalayer;
+using RB_Ärendesystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PresentationLayer.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for RegistreraJournal.xaml
+    /// </summary>
+    public partial class RegistreraJournal : Window
+    {
+       
+        public RegistreraJournal()
+        {
+            InitializeComponent();
+
+           
+
+            
+
+            
+        }
+
+        //private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Login login = new Login();
+        //    login.Show();
+        //    this.Close();
+        //    // Handle the click event for the "Log out" button
+        //    // Add logic to log out the user and navigate back to the login page
+        //}
+
+        //private void BackButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    StartSida startSida = new StartSida();
+        //    startSida.Show();
+        //    this.Close();
+        //    // Handle the click event for the "Back" button
+        //    // Add logic to navigate back to the previous page
+        //}
+
+        ////private void BesökDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        ////{
+        ////    // Get the selected row
+        ////    if (BesökDataGrid.SelectedItem != null)
+        ////    {
+
+        ////        Besök selectedBesök = (Besök)BesökDataGrid.SelectedItem;
+
+        ////        // Populate TextBoxes with values from the selected row
+
+        ////        BesökID.Text = selectedBesök.ID.ToString();
+
+                
+
+        ////    }
+
+
+        ////}
+
+        ////private void PopulateReservPartListBox()
+        ////{
+
+        ////    var journalItems = tabeller.JournalTabell().ToList();
+
+        ////        // Fetch the list of mechanics from the database
+        ////   var reservdel = tabeller.ReservdellTabell().ToList();
+
+        ////    var reservdelIdsInJournal = journalItems.Select(j => j.ID).ToList();
+
+        ////    // Filter reservdel based on ReservdelIds not present in the journalItems
+        ////    var reservdelFiltered = reservdel.Where(r => !reservdelIdsInJournal.Contains(r.ID)).ToList();
+
+
+
+        ////    // Bind the list to the ComboBox
+        ////    Reservdel.ItemsSource = reservdelFiltered;
+
+            
+
+        ////    // Set the display member path to a property of Mekaniker class that represents the name
+        ////    Reservdel.DisplayMemberPath = "Namn"; // Replace "Name" with the actual property name in your Mekaniker class
+            
+        ////}
+
+        //private void SaveButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //if (BesökDataGrid.SelectedItem != null )
+        //    //{
+
+        //    //    List<Reservdel> selectedReserv = new List<Reservdel>();
+        //    //    foreach (var selectedItem in Reservdel.SelectedItems)
+        //    //    {
+        //    //        selectedReserv.Add((Reservdel)selectedItem);
+        //    //    }
+        //    //    Besök selectedBesök = (Besök)BesökDataGrid.SelectedItem;
+           
+        //    //    TabellController controller = new TabellController();
+        //    //    nyJournalController.AddJournal(åtgärder: åtgärder.Text, besök: selectedBesök, Reservdelar: selectedReserv);
+
+        //    //    MessageBox.Show("Data saved successfully.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        //    //}
+        //}
+
+        //private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        //{
+        //    TextBox textBox = sender as TextBox;
+        //    if (textBox != null)
+        //    {
+        //        if (textBox.Text == textBox.Name)
+        //        {
+        //            textBox.Text = "";
+        //        }
+        //    }
+        //}
+
+        //private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    TextBox textBox = sender as TextBox;
+        //    if (textBox != null)
+        //    {
+        //        if (string.IsNullOrWhiteSpace(textBox.Text))
+        //        {
+        //            textBox.Text = textBox.Name;
+        //        }
+        //    }
+        //}
+
+        private void UpdateSelectedItem(object sender, SelectionChangedEventArgs e)
+        {
+            var listSelectedItems = ((ListBox)sender).SelectedItems;
+            
+
+            RegistreraJournalModel reg = new RegistreraJournalModel();
+
+            reg.SelectedReservdelar = listSelectedItems.Cast<Reservdel>().ToList();
+        }
+    }
+
+
+
+
+}
